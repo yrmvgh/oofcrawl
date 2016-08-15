93937f869fb89a4c3fa1e0faf3abf62a30703df4
Mark Alexander <dracunos72@hotmail.com>
2016-08-15 02:15:41 -0700

    Removed hunger, and moved temple to D:1

--- crawl-ref/source/food.cc
+++ crawl-ref/source/food.cc
@@ -51,6 +51,7 @@ static void _heal_from_food(int hp_amt);
 void make_hungry(int hunger_amount, bool suppress_msg,
                  bool magic)
 {
+    return;
     if (crawl_state.disables[DIS_HUNGER])
         return;
 
