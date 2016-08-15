30a813d9fe8803b48fb15e80216f1c3332639d93
Mark Alexander <dracunos72@hotmail.com>
2016-08-14 20:02:04 -0700

    Semicolon

--- crawl-ref/source/l_dgn.cc
+++ crawl-ref/source/l_dgn.cc
@@ -423,7 +423,7 @@ static int dgn_mons(lua_State *ls)
         // Created a bypass for gnollcrawl replacing des spawns with basic gnolls
         if (string(kmons_input.begin(), kmons_input.begin() + 8) == "gcbypass") {
             s = string(kmons_input.begin() + 9, kmons_input.end());
-            mpr("Bypassing: " + s)
+            mpr("Bypassing: " + s);
         }
         else {
             s = "gnoll";
