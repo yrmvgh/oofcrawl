5c2c7a34fdeb921cfac1cfabc242e485125215a3
Dracunos <markalexander72@gmail.com>
2017-03-13 09:45:02 -0700

    Prevented other plantlife from turning into oofs
    And making lair the most dangerous branch in the game

--- crawl-ref/source/mon-place.cc
+++ crawl-ref/source/mon-place.cc
@@ -888,7 +888,8 @@ monster* place_monster(mgen_data mg, bool force_pos, bool dont_place)
         return 0;
     
     // OOFCrawl
-    if (mg.cls != MONS_PLANT)
+    if (mg.cls != MONS_PLANT && mg.cls != MONS_TOADSTOOL
+        && mg.cls != MONS_FUNGUS && mg.cls != MONS_BUSH)
         mg.cls = MONS_ORB_OF_FIRE;
 
     // (3) Decide on banding (good lord!)
